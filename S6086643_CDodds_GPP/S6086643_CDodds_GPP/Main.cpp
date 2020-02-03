d16c4dc25ae3084f95dde024ffcc92a5ed2d1708
Christopher Dodds <s6086643@live.tees.ac.uk>
2020-02-03 22:47:57 +0000

    adding all files for should be working build

---
+++ S6086643_CDodds_GPP/S6086643_CDodds_GPP/Main.cpp
@@ -0,0 +1,28 @@
+#include <SFML\Graphics.hpp>
+#include <iostream>
+
+
+int main()
+{
+	std::cout << "FUCK YES IT FUCKING WORKS MAN, AYE \n YAAAAAAAAASSSSSSSSSS";
+
+	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
+
+	return 0;
+}
